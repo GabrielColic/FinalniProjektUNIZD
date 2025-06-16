60ea7d598b11d8c79eba645dfffcb17453020c7b
Gabriel <gabre747@gmail.com>
2025-06-16 16:53:40 +0200

    Improved the text editor and fixed content loading on edit screen

--- WebNovels/Models/Chapter.cs
+++ WebNovels/Models/Chapter.cs
@@ -10,7 +10,6 @@ namespace WebNovels.Models
         [Required]
         public string Title { get; set; }
 
-        [Required]
         public string Content { get; set; }
 
         public int Order { get; set; }
